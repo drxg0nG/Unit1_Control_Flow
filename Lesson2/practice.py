49dd5bf227181113bf3758a902c5116d167c6140
drxg0nG <kaavishdeepan@gmail.com>
2025-09-19 12:36:59 -0400

    homework donw

--- Lesson2/practice.py
+++ Lesson2/practice.py
@@ -1,27 +1,39 @@
-age = input("Enter Your Age: ")
+age = int(input("Enter Your Age: "))
 rating = input("Enter The Rating: ")
-rate = "G", "PG", "PG-13", "R"
-G = 0
-PG = 6
-PG13 = 13
-R = 17
+rate = ["G", "PG", "PG13", "R"]
+G = int(0)
+PG = int(6)
+PG13 = int(13)
+R = int(17)
 
 
 if not age:
     print("ERROR: Enter a valid age")
 else:
-    int(age)
-    if rating != rate:
+    if rating not in rate:
         print("ERROR: Enter a valid rating")
     else:
-        if age > G:
-            print()
-        elif age > PG:
-            print()
-        elif age > PG13:
-            print()
-        elif age > R:
-            print()
+        print("Valid Inputs")
+        if rating == "R":
+            if age >= R:
+                print("Approved! You can watch R rated films")
+            else:
+                print(f"Denied! You must be 17 years old to watch R rated films")
+        elif rating == "PG13":
+            if age >= PG13:
+                print("Approved! You can watch PG-13 rated films")
+            else:
+                print(f"Denied! You must be 13 years old to watch PG-13 rated films")
+        elif rating == "PG":
+            if age >= PG:
+                print("Approved! You can watch PG rated films")
+            else:
+                print(f"Denied! You must be 6 years old to watch PG rated films")
+        elif rating == "G":
+            if age >= G:
+                print("Approved! You can watch G rated films")
+            else:
+                print(f"Denied! You must be 0 years old to watch G rated films")
         else:
-            print()
+            print("You haven't been born yet")
 
