2af99bfe372f34a66752352e759350d9d8e35056
drxg0nG <kaavishdeepan@gmail.com>
2025-09-26 08:56:02 -0400

    lesson 5

---
+++ Lesson5-String_Iteration/strings.py
@@ -0,0 +1,46 @@
+# ======== String Indexing ========
+word = "Python"
+#       012345 (positive indexing)
+word[0] # P (first character)
+word[1] # y (second character)
+word[5] # n (last character)
+word[-1] # n (last character)
+len(word) # length of string
+word[len(word) - 1] # n (last character)
+
+# ======== String Slicing ========
+word[0:3] # Pyt (characters 0, 1, 2)
+word[:3] # Pyt (characters 0, 1, 2)
+word[2:5] # tho (characters 2, 3, 4)
+
+word[2:6] # thon (characters 2, 3, 4, 5)
+word[2:len(word)] # thon (characters 2, 3, 4, 5)
+word[2:] # thon (characters 2, 3, 4, 5)
+
+word[-3:] # hon (last 3 characters)
+
+
+# ======== Part 1 - String Iteration Patterns ========
+# Direct Character Iteration
+word = 'hello'
+for char in word:
+    print(char)
+
+# Index based iteration
+for i in range(len(word)):
+    print(f"Character {i}: {word[i]}")
+
+# Charcter Classification
+char = input("Press a key: ")
+# Check character types using ASCII ranges
+if 'A' <= char <= 'Z':
+    print(f"'{char}' is an uppercase")
+
+if 'a' <= char <= 'z':
+    print(f"'{char}' is a lowercase")
+
+if '0' <= char <= '9':
+    print(f"'{char}' is a digit")
+
+if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+    print(f"{char} is a letter")
