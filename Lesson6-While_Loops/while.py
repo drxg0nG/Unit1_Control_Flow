90813c012b92a08c9ba7977663cab6561d87ee23
drxg0nG <kaavishdeepan@gmail.com>
2025-10-01 08:56:19 -0400

    Lesson 6

---
+++ Lesson6-While_Loops/while.py
@@ -0,0 +1,44 @@
+# Syntax
+'''
+while condition:
+    code block
+    increment / decrement variable
+'''
+
+# num = 5
+# while num > 0:
+#     print(num)
+#     num -= 1
+
+num = 1
+sum = 0
+# while num <= 10:
+#     sum = sum + num
+#     num += 1
+# print(f"Sum of #'s 1-10 = {sum}")
+
+while num <= 10:
+    sum = sum + num
+    if num < 10:
+        print(num, end = " + ")
+    else:
+        print(num, end = " = ")
+    num += 1
+print(f"{sum}")
+# print(f"{sum}")
+
+# Sum of digits
+# Take a user input as int, and sum of the digits in it
+number_input = input("Enter a number: ")
+sum = 0
+# for char in number_input:
+#     # print(f"{char} {type(char)}")
+#     number = int(char)
+#     sum += number
+# print(sum)
+
+i = 0
+while i < len(number_input):
+    sum += int(number_input[i])
+    i += 1
+print(f"Total: {sum}")
