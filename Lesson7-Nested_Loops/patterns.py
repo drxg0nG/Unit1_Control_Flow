06852c621472917ad85c2ca41c48c237954a3011
drxg0nG <kaavishdeepan@gmail.com>
2025-10-10 09:22:08 -0400

    Class

--- Lesson7-Nested_Loops/patterns.py
+++ Lesson7-Nested_Loops/patterns.py
@@ -7,6 +7,7 @@
 # Row 3: 1 spaces, 7 star
 # Row 4: 0 spaces, 9 star
 
+# ======== My way ========
 rows = 5
 cols = 9
 spaces = 4
@@ -17,3 +18,17 @@ for i in range(rows):
         spaces -= 1
         rest += 2
         break
+
+# ======== Teacher way ========
+rows = 5
+# Step 1: Crete an outer loop for the rows
+for i in range(rows):
+    # Step 2: Print the spaces (rows - i - 1)
+    for j in range(rows - i - 1):
+        print(" ", end="")
+    # Step 3: Print the stars (2 * i + 1)
+    for k in range(2 * i + 1):
+        print("*", end="")
+    # Step 4: Print new line
+    print()
+
